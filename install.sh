49322225ecff63b36e68f160975a800b191aed86
jbruce <jbruce12000@gmail.com>
2015-04-28 12:43:44 -0400

    - check for r10k gem before install

--- install.sh
+++ install.sh
@@ -5,21 +5,28 @@ echo "This script requires root privileges, you will be asked your sudo password
 # Setup PuppetLabs repository
 DISTRO=$(grep DISTRIB_CODENAME /etc/upstream-release/lsb-release|sed 's/.*=//')
 echo "Distro appears to be $DISTRO..."
+PUPPET_RELEASE=$(puppetlabs-release-$DISTRO.deb)
 if ! dpkg -l puppetlabs-release ; then
-  echo "Grabbing puppet release..."
+  echo "OK Grabbing puppet release $PUPPET_RELEASE..."
   wget -q https://apt.puppetlabs.com/puppetlabs-release-$DISTRO.deb
   sudo dpkg -i puppetlabs-release-$DISTRO.deb
   sudo apt-get update -y -q
   rm -f puppetlabs-release-$DISTRO.deb 2>/dev/null
 else
-  echo "puppet release already installed, skipping"
+  echo "OK puppet release $PUPPET_RELEASE already installed, skipping"
 fi
 
 # Install puppet without the agent init script
 sudo apt-get install git puppet-common hiera -y -q
 
-# Get & run librarian-puppet
-sudo gem install r10k 
+# Get r10k
+if gem list |grep r10k ; then
+  echo "OK skipping gem install r10k"
+else
+  echo "OK installing r10k"
+  sudo gem install r10k 
+fi
+
 # Download uboxen code
 cd /opt
 [ ! -d /opt/ubuntu-boxen ] && sudo git clone https://github.com/jbruce12000/ubuntu-boxen.git
