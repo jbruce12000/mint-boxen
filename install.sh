a3270940880ae41f1bb32492b2e0e16070a4d14a
jbruce <jbruce12000@gmail.com>
2015-04-28 11:02:19 -0400

    - release did not work for mint

--- install.sh
+++ install.sh
@@ -2,9 +2,8 @@
 
 echo "This script requires root privileges, you will be asked your sudo password"
 
-
 # Setup PuppetLabs repository
-DISTRO=$(grep DISTRIB_CODENAME /etc/lsb-release | awk -F= '{print $2}')
+DISTRO=$(grep ppa /etc/apt/sources.list |grep main |head -1|cut -d " " -f 3)
 wget -q https://apt.puppetlabs.com/puppetlabs-release-$DISTRO.deb
 sudo dpkg -i puppetlabs-release-$DISTRO.deb
 sudo apt-get update -y -q
